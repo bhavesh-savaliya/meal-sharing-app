f3c4b66f94b70ae7c9e24bfb9beb7d89d1f88c71
Bhavesh Savaliya <bhavesh.savaliya@outlook.com>
2024-02-11 13:09:56 -0500

    Implement bottom navigation controls with fragments

--- app/src/main/java/com/capstone/group6/MainActivity.kt
+++ app/src/main/java/com/capstone/group6/MainActivity.kt
@@ -2,10 +2,22 @@ package com.capstone.group6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.capstone.group6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.mainContainer.id) as NavHostFragment
+        navController = navHostFragment.navController;
+
+        setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
